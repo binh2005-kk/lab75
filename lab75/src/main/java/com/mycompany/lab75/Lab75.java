15526b6b068a4a29178f001602fa9a4b855e4a27
quan <quan93909@gmail.com>
2025-03-12 15:15:21 +0700

    Crete

---
+++ lab75/src/main/java/com/mycompany/lab75/Lab75.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab75;
+
+/**
+ *
+ * @author ACER
+ */
+public class Lab75 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
